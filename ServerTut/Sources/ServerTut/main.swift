a6c62ea4f37551b127d494ad3a2d27052114b436
Boyan Yankov <boyan.yankov@igsoft.com>
2018-02-20 14:03:50 +0200

    added server start

--- ServerTut/Sources/ServerTut/main.swift
+++ ServerTut/Sources/ServerTut/main.swift
@@ -3,6 +3,19 @@ import PerfectLib
 import PerfectHTTP
 import PerfectHTTPServer
 
-Logger.enableLogging(true)
+#if DEBUG
+    Logger.enableLogging(true)
+#endif
 
-print("Hello, world!")
+
+let server: HTTPServer = HTTPServer()
+server.serverPort = 8181
+server.documentRoot = "webRoot"
+
+
+do {
+    try server.start()
+}
+catch PerfectError.networkError(let err, let msg) {
+    Logger.error.message("Network error: \(err) \(msg)")
+}
