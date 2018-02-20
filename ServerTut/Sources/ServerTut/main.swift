3cbe26c4efbd4ee3fd4173e94d77e7bcd7eb96f7
Boyan Yankov <boyan.yankov@igsoft.com>
2018-02-20 14:36:56 +0200

    added json router

--- ServerTut/Sources/ServerTut/main.swift
+++ ServerTut/Sources/ServerTut/main.swift
@@ -9,14 +9,35 @@ import PerfectHTTPServer
 
 // MARK: - Setup server
 let server: HTTPServer = HTTPServer()
-server.serverPort = 8181
-server.documentRoot = "webRoot"
+server.serverPort = AppConstants.ServerParameters.port
+server.documentRoot = AppConstants.ServerParameters.rootDirectory
 
 // MARK: - Routes
 var routes: Routes = Routes()
+
+// plain text route
 routes.add(method: .get, uri: "/") { (request, response) in
-    response.setBody(string: "Plain text response").completed()
+    response.setBody(string: "Plain text response")
+        .completed()
 }
+
+// json route
+func returnJSONMessage(message: String, response: HTTPResponse) {
+    do {
+        try response.setBody(json: ["message": message])
+            .setHeader(HTTPResponseHeader.Name.contentType, value: AppConstants.HeaderValues.applicationJSON)
+            .completed()
+    }
+    catch {
+        response.setBody(string: "Error handling request: \(error)")
+            .completed(status: .internalServerError)
+    }
+}
+
+routes.add(method: .get, uri: "/json") { (request, response) in
+    returnJSONMessage(message: "Hello, JSON!", response: response)
+}
+
 server.addRoutes(routes)
 
 // MARK: - Start server
