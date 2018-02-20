6b28f23f7b0ccc4718daa89aab1a3a1266485060
Boyan Yankov <boyankov@yahoo.com>
2018-02-20 16:16:10 +0200

    added http server

--- ExecutableProjectAssistent/Package.swift
+++ ExecutableProjectAssistent/Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version:4.0
 // Generated automatically by Perfect Assistant 2
-// Date: 2018-02-20 14:13:38 +0000
+// Date: 2018-02-20 14:14:39 +0000
 import PackageDescription
 
 let package = Package(
@@ -9,6 +9,10 @@ let package = Package(
 		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.0.0"..<"4.0.0")
 	],
 	targets: [
-		.target(name: "ExecutableProjectAssistent", dependencies: [])
+		.target(
+            name: "ExecutableProjectAssistent",
+            dependencies: [
+                "PerfectHTTPServer"
+            ])
 	]
 )
