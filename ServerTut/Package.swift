51325558919e8bca559d4e68f82a46eaa5e8e5f3
Boyan Yankov <boyan.yankov@igsoft.com>
2018-02-20 12:18:23 +0200

    updated packages

--- ServerTut/Package.swift
+++ ServerTut/Package.swift
@@ -8,12 +8,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        Package.Dependency.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: Version(3, 0, 5))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ServerTut",
-            dependencies: []),
+            dependencies: [
+                "PerfectHTTPServer"
+            ])
     ]
 )
