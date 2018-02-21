774c2c1df93f405196dbaf1fe6b37b2c7cebd499
Boyan Yankov <boyankov@yahoo.com>
2018-02-21 13:09:04 +0200

    updated `main.swift` - added server setup

--- til_tut/Sources/main.swift
+++ til_tut/Sources/main.swift
@@ -1,1 +1,26 @@
-debugPrint("🔧 \(#file) » \(#function) » \(#line)", "Hello, World", separator: "\n")
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+import StORM
+import PostgresStORM
+
+PostgresConnector.host = "localhost"
+PostgresConnector.username = "pguser1"
+PostgresConnector.password = "p@ssword"
+PostgresConnector.database = "til_db_1"
+PostgresConnector.port = 5432
+
+let setupObj = Acronym()
+try? setupObj.setup()
+
+let server = HTTPServer()
+server.serverPort = 8080
+
+let basic = BasicController()
+server.addRoutes(Routes(basic.routes))
+
+do {
+    try server.start()
+} catch PerfectError.networkError(let err, let msg) {
+    print("Network error thrown: \(err) \(msg)")
+}
