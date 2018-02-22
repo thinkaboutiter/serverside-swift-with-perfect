c915bf99e7c34c1354ee454b1e85284acde28db6
Boyan Yankov <boyankov@yahoo.com>
2018-02-22 15:19:26 +0200

    updated `BasicController.swift` - added creation api

--- ShoppingList/Sources/BasicController.swift
+++ ShoppingList/Sources/BasicController.swift
@@ -15,7 +15,8 @@ final class BasicController {
     // MARK: - Properties
     var routes: [Route] {
         return [
-            Route(method: .get, uri: "/all", handler: all)
+            Route(method: .get, uri: "/all", handler: all),
+            Route(method: .post, uri: "/shoppingItem", handler: create)
         ]
     }
     
@@ -23,6 +24,18 @@ final class BasicController {
     func all(request: HTTPRequest, response: HTTPResponse) {
         do {
             let json: String = try ShoppingItem.allAsString()
+            response.setBody(string: json)
+                .setHeader(.contentType, value: "application/json")
+                .completed()
+        } catch {
+            response.setBody(string: "Error handling request: \(error)")
+                .completed(status: .internalServerError)
+        }
+    }
+    
+    func create(request: HTTPRequest, response: HTTPResponse) {
+        do {
+            let json = try ShoppingItem.create(withJSONRequest: request.postBodyString)
             response.setBody(string: json)
                 .setHeader(.contentType, value: "application/json")
                 .completed()
