bd8e15899b0ad151e42355027725980fb1d0d902
Boyan Yankov <boyankov@yahoo.com>
2018-02-22 15:50:11 +0200

    added starter project template `WebSockets` from Perfect

---
+++ BubblyChat/server/BubblyChatServer/Sources/main.swift
@@ -0,0 +1,42 @@
+//
+//  UploadHandler.swift
+//  Upload Enumerator
+//
+//  Created by Kyle Jessup on 2015-11-05.
+//	Copyright (C) 2015 PerfectlySoft, Inc.
+//
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Perfect.org open source project
+//
+// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
+// Licensed under Apache License v2.0
+//
+// See http://perfect.org/licensing.html for license information
+//
+//===----------------------------------------------------------------------===//
+//
+
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+
+// Create HTTP server
+let server = HTTPServer()
+
+// Set the server's webroot
+server.documentRoot = "./webroot"
+
+// Add our routes and such
+let routes = makeRoutes()
+server.addRoutes(routes)
+
+// Listen on port 8181
+server.serverPort = 8181
+
+do {
+    // Launch the HTTP server on port 8181
+    try server.start()    
+} catch PerfectError.networkError(let err, let msg) {
+    print("Network error thrown: \(err) \(msg)")
+}
