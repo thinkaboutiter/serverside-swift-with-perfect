f66deac358ad6ffaab46596969a2bf4b5cae30c1
Boyan Yankov <boyankov@yahoo.com>
2018-07-19 23:10:17 +0300

    updated `Package.swift` using types initializers

--- ShoppingList/Package.swift
+++ ShoppingList/Package.swift
@@ -1,11 +1,24 @@
-// Generated automatically by Perfect Assistant Application
-// Date: 2018-02-21 14:27:42 +0000
+// manual creation
+//
 import PackageDescription
+
 let package = Package(
 	name: "ShoppingList",
-	targets: [],
-	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
-		.Package(url: "https://github.com/SwiftORM/MongoDB-StORM.git", majorVersion: 3),
-	]
+	targets: [
+        Target(
+            name: "ShoppingList",
+            dependencies: [
+                Target.Dependency.Target(name: "MongoDBStORM"),
+                Target.Dependency.Target(name: "PerfectHTTPServer"),
+                Target.Dependency.Target(name: "SimpleLogger")
+            ])
+    ],
+    dependencies: [
+        Package.Dependency.Package(url: "https://github.com/SwiftORM/MongoDB-StORM.git",
+                                   versions: Version(3,0,0)..<Version(3,9,9)),
+        Package.Dependency.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git",
+                                   versions: Version(3,0,0)..<Version(3,9,9)),
+        Package.Dependency.Package(url: "https://github.com/thinkaboutiter/SimpleLogger.git",
+                                   versions: Version(1,0,0)..<Version(1,9,9))
+            ]
 )
