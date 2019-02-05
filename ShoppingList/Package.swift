736770f0f5ca6918f2aa71fa81377f56427a4263
boyankov <boyan.yankov@yahoo.com>
2019-02-06 01:15:18 +0200

    updated package file

--- ShoppingList/Package.swift
+++ ShoppingList/Package.swift
@@ -1,17 +1,29 @@
+// swift-tools-version:4.0
 // manual creation
 //
 import PackageDescription
 
 let package = Package(
     name: "ShoppingList",
-    targets: [
+    products: [
+        .executable(
+            name: "ShoppingList",
+            targets: ["ShoppingList"]
+        ),
     ],
     dependencies: [
-        Package.Dependency.Package(url: "https://github.com/SwiftORM/MongoDB-StORM.git",
-                                   versions: Version(3,0,0)..<Version(3,9,9)),
-        Package.Dependency.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git",
-                                   versions: Version(3,0,0)..<Version(3,9,9)),
-//        Package.Dependency.Package(url: "https://github.com/thinkaboutiter/SimpleLogger.git",
-//                                   versions: Version(1,0,0)..<Version(1,9,9))
-        ]
+        .package(url: "https://github.com/SwiftORM/MongoDB-StORM.git", from: "3.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+//        .package(url: "https://github.com/thinkaboutiter/SimpleLogger.git", from: "1.0.0")
+    ],
+    targets: [
+        .target(
+            name: "ShoppingList",
+            dependencies: [
+                "PerfectHTTPServer",
+                "MongoDBStORM",
+            ],
+            path: "Sources"
+        ),
+    ]
 )
