e535373aaa4c1cd0bfecfe8f9b629964065e7f95
boyankov <boyan.yankov@yahoo.com>
2019-02-06 02:31:47 +0200

    updated `Package.swift` - added paths

--- ShoppingList/Package.swift
+++ ShoppingList/Package.swift
@@ -14,9 +14,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ShoppingList",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "ShoppingListTests",
-            dependencies: ["ShoppingList"]),
+            dependencies: ["ShoppingList"],
+            path: "Tests"),
     ]
 )
