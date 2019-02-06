d460d2d6e68a97b2a57158a2967a5d84cd35d990
boyankov <boyan.yankov@yahoo.com>
2019-02-06 04:13:00 +0200

    updated `Package.swift` - added dependencies

--- ShoppingList/Package.swift
+++ ShoppingList/Package.swift
@@ -6,15 +6,17 @@ import PackageDescription
 let package = Package(
     name: "ShoppingList",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/SwiftORM/MongoDB-StORM.git", from: "3.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+        // .package(url: "https://github.com/thinkaboutiter/SimpleLogger.git", from: "1.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ShoppingList",
-            dependencies: [],
+            dependencies: [
+                "PerfectHTTPServer",
+                "MongoDBStORM",
+            ],
             path: "Sources"),
     ]
 )
